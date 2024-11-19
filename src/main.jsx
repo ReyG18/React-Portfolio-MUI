7481213f986f8922bd59d69956aa6d3b6195bcc9
Rey G. <reyanna.garibay@gmail.com>
2024-11-19 15:18:00 -0600

    Basic theme file set up

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
+import theme from "./theme/theme";
 import App from "./App";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+    <CssBaseline /> {/* css reset */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
