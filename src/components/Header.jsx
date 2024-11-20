e818d31bfa29a3fd125e9fa8411cac3919bbef93
Rey G. <reyanna.garibay@gmail.com>
2024-11-19 18:11:52 -0600

    General styling of Appbar, no tab styling yet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+
 
 export default function Header() {
   return(
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/contact">Contact</Link>
-    </nav>
+    <AppBar position="static">
+      <Container maxWidth="x1">
+        <Toolbar disableGutters>
+          <Typography variant="h4" 
+            sx={{ 
+              py: 5,
+              pr: 10,
+              display: { xs: 'none', md: 'flex'} 
+            }}
+            >
+              Reyanna Garibay
+          </Typography>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/contact">Contact</Link>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
