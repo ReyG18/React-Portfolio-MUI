ccac23ef0f7c45c171f5eb738995ce8b4e0baf78
Rey G. <reyanna.garibay@gmail.com>
2024-11-20 16:27:08 -0600

    links for iconButtons work

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import { GitHub, LinkedIn, Email } from "@mui/icons-material";
 import { AppBar, IconButton, Toolbar } from "@mui/material"
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return(
     <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }} >
       <Toolbar disableGutters sx={{ justifyContent: "center" }}>
-        <IconButton aria-label="github">
+        <IconButton aria-label="github" component="a" href="https://github.com/ReyG18">
           <GitHub />
         </IconButton>
-        <IconButton aria-label="linkedIn">
+        <IconButton aria-label="linkedIn" component="a" href="https://linkedin.com/in/reyg18">
           <LinkedIn />
         </IconButton>
         <IconButton aria-label="email">
