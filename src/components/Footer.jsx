638aec6b2f9c8f7fe42756132a112562ca7733aa
Rey G. <reyanna.garibay@gmail.com>
2024-11-20 16:13:50 -0600

    basic header w/icons, no links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
-// import { GitHub, LinkedIn, Email } from "@mui/icons-material";
+import { GitHub, LinkedIn, Email } from "@mui/icons-material";
+import { AppBar, IconButton, Toolbar } from "@mui/material"
 
 export default function Footer() {
   return(
-    <h4>Footer</h4>
+    <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }} >
+      <Toolbar disableGutters sx={{ justifyContent: "center" }}>
+        <IconButton aria-label="github">
+          <GitHub />
+        </IconButton>
+        <IconButton aria-label="linkedIn">
+          <LinkedIn />
+        </IconButton>
+        <IconButton aria-label="email">
+          <Email />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { AppBar, Box, Container, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import { AppBar, Container, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 
